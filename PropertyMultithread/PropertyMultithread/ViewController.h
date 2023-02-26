4eb09c434769bcd9b59a0ae481466654fbb12516
zeddwu <zeddwu@tencent.com>
2023-02-26 16:29:02 +0800

    initial Property Multithreading experiment

---
+++ PropertyMultithread/PropertyMultithread/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PropertyMultithread
+//
+//  Created by zeddwu on 2023/2/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
